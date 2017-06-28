b5f9240dd5754476ee9bfa2761e5e51876378ac5
tono0123 <e080514@gmail.com>
2017-06-28 22:34:31 +0900

    add create vm and image token

--- create_vm.rb
+++ create_vm.rb
@@ -1,2 +1,20 @@
 require 'bundler/setup'
-require ''
+require 'json'
+require 'rest-client'
+
+url = "https://compute.tyo1.conoha.io/v2/{tenant_id}/servers"
+headers = {
+	"X-Auth-Token": usertoken,
+	"Accept": "application/json"
+}
+
+payload = {
+	"server": {
+		"imageRef": "8c9f7e23-c42e-4eeb-9672-786d80ce84aa",
+		"flavorRef": "7eea7469-0d85-4f82-8050-6ae742394681"
+	}
+}
+
+response = RestClient.post(url, payload.to_json, headers)
+p res = JSON.parse(response.body)
+
